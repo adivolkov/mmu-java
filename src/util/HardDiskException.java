d39dc3568b7ddb4f6c63ecd620ab2153ed241d00
volkovitz <volkovitz@RLZ-L-VOLKOVITZ.mshome.net>
2014-09-05 23:18:38 +0300

    +HD Classes

---
+++ src/util/HardDiskException.java
@@ -0,0 +1,12 @@
+package util;
+
+public class HardDiskException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public HardDiskException(){
+		System.out.println("HardDisk Exception!");
+	}
+}
